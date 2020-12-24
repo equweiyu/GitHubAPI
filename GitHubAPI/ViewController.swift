078900bf4b4de67caa1ff07100bae442a3f3acec
Bai Tianwei <equweiyu@hotmail.com>
2020-12-24 09:35:53 +0800

    init

---
+++ GitHubAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubAPI
+//
+//  Created by 白天伟 on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
